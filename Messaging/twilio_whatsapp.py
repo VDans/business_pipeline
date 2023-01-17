8e82eedd743dfc23b492e5344c352be7d59df337
P015650 <valentin.dans@bawagpsk.com>
2023-01-17 15:34:00 +0100

    Whatsapp Sender now working

--- Messaging/twilio_whatsapp.py
+++ Messaging/twilio_whatsapp.py
@@ -1,37 +1,45 @@
-# import json
-#
-# from flask import Flask, request, redirect
-# from twilio.twiml.messaging_response import MessagingResponse
-# from twilio.rest import Client
-#
-# secrets = json.load(open('../config_secrets.json'))
-#
-#
-# class Whatsapp:
-#     def __init__(self):
-#         self.account_sid = secrets['twilio']['account_sid']
-#         self.auth_token = secrets['twilio']['auth_token']
-#
-#         self.client = Client(self.account_sid, self.auth_token)
-#
-#     def send_whatsapp_message(self, approved=False):
-#         if not approved:
-#             m_template = self.client.messages.create(
-#                 from_='whatsapp:+17816307516',
-#                 to='whatsapp:+436601644192',
-#                 body=   """Bonjour !
-#                         Je suis Valentin, votre hôte pour Vienne.
-#                         Nous avons bien reçu votre réservation, et vous recevrez des informations sur l'enregistrement peu avant votre arrivée.
-#                         Veuillez utiliser ce numéro si vous avez des questions !
-#                         Bien à vous,
-#                         Valentin
-#                         """
-#             )
-#
-#         else:
-#             m_approved = self.client.messages.create(
-#                 from_='whatsapp:+17816307516',
-#                 media_url=["https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/how-to-keep-ducks-call-ducks-1615457181.jpg"],
-#                 body="""""",
-#                 to='whatsapp:+436601644192'
-#             )
+from twilio.rest import Client
+
+
+class Whatsapp:
+    def __init__(self, secrets, resources):
+        self.secrets = secrets
+        self.resources = resources
+        self.account_sid = self.secrets['twilio']['account_sid']
+        self.auth_token = self.secrets['twilio']['auth_token']
+
+        self.client = Client(self.account_sid, self.auth_token)
+
+    def send_whatsapp_message(self):
+        m_template = self.client.messages.create(
+            from_=self.resources["twilio"]["whatsapp_sender"],
+            to="whatsapp:+436601644192",
+            body="""Hello!
+
+I hope you are doing well!
+Hereby some help for the way to your apartment.
+
+Please come to Erdbergstrasse 118, 1030 Vienna. The closest station is Kardinal-Nagl Platz, 50 meters away.
+Please ring at number 32, with Kasbacher written on it. After 3 seconds, you will be let in automatically.
+Please go to the 3rd floor, and then left, until you find a large white door with the number 32.
+There, on the door you will see a code pad. Enter the 6 digits code, and the door will open for you.
+
+Once in the apartment, a physical key will be waiting for you on the kitchen counter.
+
+Have a very nice trip, and enjoy Vienna!
+
+Kind regards, 
+Valentin"""
+        )
+
+        """
+        HOW TO SEND A PICTURE
+        else:
+            m_approved = self.client.messages.create(
+                from_='whatsapp:+17816307516',
+                media_url=["https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/how-to-keep-ducks-call-ducks-1615457181.jpg"],
+                body="""""",
+                to='whatsapp:+436601644192'
+            )
+        """
+        return m_template
