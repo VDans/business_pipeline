03053c9cb4766cff97493ee065ae9e09278a0982
Valentin D'ans <valen@live.be>
2023-01-18 01:51:21 +0100

    Transfer of the messages to Whatsapp

--- Messaging/twilio_whatsapp.py
+++ Messaging/twilio_whatsapp.py
@@ -43,16 +43,19 @@ class Whatsapp:
         """
         return m_template
 
-    def text_cleaner(self, event: str, unit_id: str, job_date: pd.Timestamp, cleaner_phone_number: str, next_guests_n_nights=None, next_guests_n_guests=None):
-        if event == "reservation":
-            out1 = "Veränderung"
+    def message_cleaner(self, event: str, unit_id: str, job_date: pd.Timestamp, cleaner_phone_number: str, next_guests_n_nights=None, next_guests_n_guests=None):
+        if event == "new":
+            out1 = "Neue Buchung"
             body = f"{out1} in {unit_id}\nDatum: {job_date.strftime('%Y-%m-%d')}\nAnzahl Gäste: {next_guests_n_guests}\nAnzahl Nächte: {next_guests_n_nights}"
-        elif event == "modification of booking":
-            out1 = "Veränderung"
+
+        elif event == "change":
+            out1 = "Änderung"
             body = f"{out1} in {unit_id}\nDatum: {job_date.strftime('%Y-%m-%d')}\nAnzahl Gäste: {next_guests_n_guests}\nAnzahl Nächte: {next_guests_n_nights}"
-        elif event == "cancellation":
+
+        elif event == "cancel":
             out1 = "Stornierung"
             body = f"{out1} in {unit_id}\nDatum: {job_date.strftime('%Y-%m-%d')}\nKeine Hilfe mehr nötig."
+
         else:
             body = None
             out1 = None
@@ -60,5 +63,16 @@ class Whatsapp:
 
         m_template = self.client.messages.create(
             from_=self.resources["twilio"]["whatsapp_sender"],
+            to=f"whatsapp:{cleaner_phone_number}",
+            body=body)
+
+        return m_template
+
+    def message_owner(self, event, unit_id, name, from_date, to_date, phone):
+        body = f"{event}: {name}\nFlat: {unit_id}\nCheck-In: {from_date.strftime('%Y-%m-%d')} \nCheck-Out: {to_date.strftime('%Y-%m-%d')} \nPhone: {phone}"
+        m_template = self.client.messages.create(
+            from_=self.resources["twilio"]["whatsapp_sender"],
             to="whatsapp:+436601644192",
-            body="YOUR MESSAGE HERE")
+            body=body)
+
+        return m_template
--- flask_webhook.py
+++ flask_webhook.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 import logging
 from flask import Flask, request, json
@@ -40,13 +42,15 @@ def receive_data_smoobu():
 
     verify_signature()
 
-    event_type = data["data"]["type"]  # FixMe: This is not the correct key to use and leads only to modifications!
+    event_type = data["action"]
     unit_id = data["data"]["apartment"]["name"]
     guest_name = data["data"]["guest-name"].title()
     check_in = pd.Timestamp(data["data"]["arrival"])  # String
     check_out = pd.Timestamp(data["data"]["departure"])  # String
     adults = data["data"]["adults"]
     children = data["data"]["children"]
+    n_nights = (check_out - check_in).days
+    phone = data["data"]["phone"]
 
     logging.info(f"Event: {event_type}")
     logging.info(f"Unit ID: {unit_id}")
@@ -55,195 +59,49 @@ def receive_data_smoobu():
     logging.info(f"Check-Out: {check_out}")
     logging.info(f"adults: {adults}")
     logging.info(f"children: {children}")
+    logging.info(f"Phone Number: {phone}")
 
     # Assign cleaner:
     cleaner_id = resources["apt_cleaners"][data["data"]["apartment"]["name"]]["name"]
     cleaner_phone = resources["apt_cleaners"][data["data"]["apartment"]["name"]]["phone_number"]
     logging.info(f"Cleaner Assigned: {cleaner_id}")
 
-    if event_type == "reservation":
+    if event_type == 'newReservation':
+        w.message_owner(event="New Booking", unit_id=unit_id, name=guest_name, from_date=check_in, to_date=check_out, phone=phone)
         if within_n_days(n=14, date=check_in):
-            if is_event_on_day(api_connector=s, unit_id=unit_id, checked_event="check-out", date=check_in):
-                w.text_cleaner(event="CHANGE", data={"checkin, checkout, adults, children"})
-                text_owner(event="CHANGE", data={"checkin, checkout, adults, children"})
-                update_cleanings_db("ADD or CHANGE")
+            bookings = s.get_smoobu_bookings(from_date=check_in, to_date=check_in, unit_id=unit_id, filter_by="check-out")
+            if len(bookings):
+                w.message_cleaner(event="change", unit_id=unit_id, job_date=check_in, cleaner_phone_number=cleaner_phone, next_guests_n_guests=adults + children, next_guests_n_nights=n_nights)
+                update_cleanings_db(con=db_engine, action="change_guests", n_guests=adults + children, cleaner_id=cleaner_id, job_date=check_in, unit_id=unit_id)
 
         if within_n_days(n=14, date=check_out):
-            if is_event_on_day(api_connector=s, unit_id=unit_id, checked_event="check-in", date=check_out):
-                text_cleaner(event="NEW", data={"checkin, checkout, adults, children from request_booking"})
-                text_owner(event="NEW", data={"checkin, checkout, adults, children from request_booking"})
-                update_cleanings_db("ADD")
+            bookings = s.get_smoobu_bookings(from_date=check_out, to_date=check_out, unit_id=unit_id, filter_by="check-in")
+            if len(bookings):
+                next_check_out = pd.Timestamp(bookings["departure"][0])
+                next_check_in = pd.Timestamp(bookings["arrival"][0])
+                next_nights = (next_check_out - next_check_in).days
+                next_guests = int(bookings['adults'][0]) + int(bookings['children'][0])
+                w.message_cleaner(event="new", unit_id=unit_id, job_date=check_out, cleaner_phone_number=cleaner_phone, next_guests_n_guests=next_guests, next_guests_n_nights=next_nights)
+                update_cleanings_db(con=db_engine, action="add", n_guests=next_guests, cleaner_id=cleaner_id, job_date=check_out, unit_id=unit_id)
             else:
-                text_cleaner(event="NEW", data={"checkin, checkout, adults, children from MAX capacity"})
-                text_owner(event="NEW", data={"checkin, checkout, adults, children from MAX capacity"})
-                update_cleanings_db("ADD")
+                max_guests = resources["apt_max_occupancy"][unit_id]
+                max_nights = 3
+                w.message_cleaner(event="new", unit_id=unit_id, job_date=check_out, cleaner_phone_number=cleaner_phone, next_guests_n_guests=max_guests, next_guests_n_nights=max_nights)
+                update_cleanings_db(con=db_engine, action="add", n_guests=max_guests, cleaner_id=cleaner_id, job_date=check_out, unit_id=unit_id)
 
-    elif event_type == 'modification of booking':
-        # Are you able to know what has changed? Add here. Otherwise, like new booking.
-        pass
-
-    elif event_type == "cancellation":
+    elif event_type == 'cancelReservation':
+        w.message_owner(event="Cancellation", unit_id=unit_id, name=guest_name, from_date=check_in, to_date=check_out, phone=phone)
         if within_n_days(n=14, date=check_in):
-            if is_event_on_day(api_connector=s, unit_id=unit_id, checked_event="check-out", date=check_in):
-                text_cleaner(event="CHANGE", data={"checkin, checkout, adults, children to MAX occupancy"})
-                text_owner(event="CANCEL + CHANGE", data={"checkin, checkout, adults, children to MAX occupancy"})
-                update_cleanings_db("CHANGE to MAX occupancy")
+            bookings = s.get_smoobu_bookings(from_date=check_in, to_date=check_in, unit_id=unit_id, filter_by="check-out")
+            if len(bookings):
+                max_guests = resources["apt_max_occupancy"][unit_id]
+                max_nights = 3
+                w.message_cleaner(event="change", unit_id=unit_id, job_date=check_in, cleaner_phone_number=cleaner_phone, next_guests_n_guests=max_guests, next_guests_n_nights=max_nights)
+                update_cleanings_db(con=db_engine, action="change_guests", n_guests=max_guests, cleaner_id=cleaner_id, job_date=check_out, unit_id=unit_id)
 
         if within_n_days(n=14, date=check_out):
-            text_cleaner(event="CANCEL", data={"canceled job date"}
-            text_owner(event="CANCEL", data={"canceled job date"}
-            update_cleanings_db("CHANGE to canceled")
-
-
-            if event_type in ["reservation", "modification of booking"]:
-
-            ##############################################
-            # Finding the previous booking on the same day
-            # Only IF check-in on the same day as previous check-out (which, by default, was set on max capacity)
-                past_booking = s.get_smoobu_bookings(from_date=check_in,
-                                                     to_date=check_in,
-                                                     unit_id=unit_id,
-                                                     filter_by="check-out")
-
-            if len(past_booking) == 0:
-                logging.info(f"No one is checking out on the day of arrival of the guest. Nothing has to be changed.")
-
-            else:
-                logging.info(f"Someone is checking out on the day of arrival of the guest. The cleaning has to be updated.")
-            # If the booking arrival is within 14 days, warn the staff.
-            if (check_in <= (pd.Timestamp.now() + pd.Timedelta(days=14))) & (check_in >= pd.Timestamp.now()):
-                sms.cleaner_sms(event=event_type,
-                                unit_id=unit_id,
-                                job_date=check_in,
-                                next_guests_n_nights=(check_out - check_in).days,
-                                next_guests_n_guests=data["data"]['adults'] + data["data"]['children'],
-                                cleaner_phone_number=cleaner_phone)
-            logging.info(f"Cleaner has been texted successfully")
-
-            # Text owner to confirm.
-            sms.new_booking_sms(event=event_type,
-                                unit=unit_id,
-                                name=guest_name,
-                                from_date=check_in,
-                                to_date=check_out,
-                                phone=data["data"]["phone"])
-
-            # The relevant cleaning therefore has to be changed.
-            logging.info(f"The cleaning right before check-in has been modified")
-
-            ##############################################
-            # Finding the NEXT booking, if on the same day
-            next_booking = s.get_smoobu_bookings(from_date=check_out,
-                                                 to_date=check_out,
-                                                 unit_id=unit_id,
-                                                 filter_by="check-in")
-
-            if len(next_booking) == 0:
-            # In this case use the max occupancy:
-                logging.info(f"No one is arriving on that check-out day. Assigning max capacity: {resources['apt_max_occupancy'][unit_id]} guests")
-            next_guests = resources["apt_max_occupancy"][unit_id]
-            next_nights = 3  # Default value
-
-            else:
-            # Record the info of the next guests
-            next_check_out = pd.Timestamp(next_booking["departure"][0])
-            next_check_in = pd.Timestamp(next_booking["arrival"][0])
-            next_nights = (next_check_out - next_check_in).days
-            next_guests = int(next_booking['adults'][0]) + int(next_booking['children'][0])
-            logging.info(f"The next booking has {next_guests} guests and stay {next_nights} nights")
-
-            # If the booking arrival is within 14 days, warn the staff.
-            if (check_out <= (pd.Timestamp.now() + pd.Timedelta(days=14))) & (check_out >= pd.Timestamp.now()):
-                sms.cleaner_sms(event=event_type,
-                                unit_id=unit_id,
-                                job_date=check_out,
-                                next_guests_n_nights=next_nights,
-                                next_guests_n_guests=next_guests,
-                                cleaner_phone_number=cleaner_phone)
-            logging.info(f"Cleaner has been texted successfully")
-
-            # Text owner to confirm.
-            sms.new_booking_sms(event=event_type,
-                                unit=unit_id,
-                                name=guest_name,
-                                from_date=check_in,
-                                to_date=check_out,
-                                phone=data["data"]["phone"])
-
-            # Cleaner has been informed, therefore add the job to the relevant db table:
-            cleaner_row = pd.DataFrame([{
-                "personnel_id": cleaner_id,
-                "job_date": check_out,
-                "received_on": pd.Timestamp.now(),
-                "unit_id": unit_id,
-                "job_type": "cleaning",
-                "status": "booked",
-                "paid": False
-            }])
-            cleaner_row.to_sql(name="cleanings",
-                               con=db_engine,
-                               if_exists="append",
-                               index=False)
-
-            elif event_type == "cancellation":
-            cleaner_id = resources["apt_cleaners"][data["data"]["apartment"]["name"]]["name"]
-            cleaner_phone = resources["apt_cleaners"][data["data"]["apartment"]["name"]]["phone_number"]
-
-            # Modify the previous cleaning to maximum occupancy:
-            past_booking = s.get_smoobu_bookings(from_date=check_in,
-                                                 to_date=check_in,
-                                                 unit_id=unit_id,
-                                                 filter_by="check-out")
-
-            if len(past_booking) == 0:
-                logging.info(f"No one is checking out on the day of arrival of the guest. Nothing has to be changed.")
-
-            else:
-                logging.info(f"Someone is checking out on the day of arrival of the guest. The cleaning has to be updated to max occupancy.")
-            # If the booking arrival is within 14 days, warn the staff.
-            if (check_in <= (pd.Timestamp.now() + pd.Timedelta(days=14))) & (check_in >= pd.Timestamp.now()):
-                sms.cleaner_sms(event="modification of booking",
-                                unit_id=unit_id,
-                                job_date=check_in,
-                                next_guests_n_nights=3,
-                                next_guests_n_guests=resources["apt_max_occupancy"][unit_id],
-                                cleaner_phone_number=cleaner_phone)
-            logging.info(f"Cleaner has been texted successfully")
-
-            # Text owner to confirm.
-            sms.new_booking_sms(event="modification of booking",
-                                unit=unit_id,
-                                name=guest_name,
-                                from_date=check_in,
-                                to_date=check_out,
-                                phone=data["data"]["phone"])
-
-            # The relevant cleaning therefore has to be changed.
-            logging.info(f"The cleaning right before check-in has been modified")
-
-            # If the booking arrival is within 14 days, warn the staff of the cancellation
-            if (check_out <= (pd.Timestamp.now() + pd.Timedelta(days=14))) & (check_out >= pd.Timestamp.now()):
-                sms.cleaner_sms(event=event_type,
-                                unit_id=unit_id,
-                                job_date=check_out,
-                                cleaner_phone_number=cleaner_phone)
-            logging.info(f"Cleaner has been texted successfully")
-
-            # Text owner to confirm.
-            sms.new_booking_sms(event=event_type,
-                                unit=unit_id,
-                                name=guest_name,
-                                from_date=check_in,
-                                to_date=check_out,
-                                phone=data["data"]["phone"])
-
-            # Cleaner has been informed, therefore change the job in the relevant db table:
-            sql = "UPDATE cleanings SET status = 'Canceled' WHERE personnel_id = %(pid)s AND unit_id = %(unit)s AND job_date = %(job_date)s"
-            db_engine.execute(sql, {'pid': cleaner_id, 'unit': unit_id, 'job_date': check_out})
-            logging.info(f"{guest_name}'s status was changed to 'Canceled'.")
-
-            else:
-            ValueError("Call Type unknown")
+            w.message_cleaner(event="cancel", unit_id=unit_id, job_date=check_out, cleaner_phone_number=cleaner_phone)
+            update_cleanings_db(con=db_engine, action="cancel", cleaner_id=cleaner_id, job_date=check_out, unit_id=unit_id)
 
     return data
 
@@ -259,18 +117,54 @@ def verify_signature():
     """
     pass
 
+
 def within_n_days(n: int, date: pd.Timestamp):
     return (date <= (pd.Timestamp.now() + pd.Timedelta(days=n))) & (date >= pd.Timestamp.now())
 
-def is_event_on_day(api_connector, unit_id, date: pd.Timestamp, checked_event: str = "check-out"):
-    f"""
-    Is there a {checked_event} on the {date}?
+
+def update_cleanings_db(con, action: str, **data):
     """
-    out = api_connector.get_smoobu_bookings(from_date=date,
-                                            to_date=date,
-                                            unit_id=unit_id,
-                                            filter_by=checked_event)
-    return len(out) > 0
+    :param con:
+    :param action: One of "add", "change_guests", "change_date" and "cancel".
+    :param data: Can contain cleaner_id, job_date, unit_id, n_guests.
+    """
+    if action == "add":
+        cleaner_row = pd.DataFrame([{
+            "personnel_id": data["cleaner_id"],
+            "job_date": data["job_date"],
+            "guests": data["n_guests"],
+            "received_on": pd.Timestamp.now(),
+            "unit_id": data["unit_id"],
+            "job_type": "cleaning",
+            "status": "booked",
+            "paid": False
+        }])
+        cleaner_row.to_sql(name="cleanings",
+                           con=con,
+                           if_exists="append",
+                           index=False)
+
+    elif action == "change_guests":
+        sql = f"""UPDATE cleanings SET guests = %(guests)s WHERE personnel_id = %(pid)s AND unit_id = %(unit)s AND job_date = %(job_date)s"""
+        db_engine.execute(sql, {'guests': data["n_guests"], 'pid': data["cleaner_id"], 'unit': data["unit_id"],
+                                'job_date': data["job_date"].strftime('%Y-%m-%d')})
+        logging.info(f"Cleaning's guests amount updated.")
+
+    elif action == "change_dates":
+        sql = f"""UPDATE cleanings SET job_date = %(job_date_new)s WHERE personnel_id = %(pid)s AND unit_id = %(unit)s AND job_date = %(job_date)s"""
+        db_engine.execute(sql, {'pid': data["cleaner_id"], 'unit': data["unit_id"],
+                                'job_date_new': data["job_date"].strftime('%Y-%m-%d')})
+        logging.info(f"Cleaning's date updated.")
+
+    elif action == "cancel":
+        sql = "UPDATE cleanings SET status = 'Canceled' WHERE personnel_id = %(pid)s AND unit_id = %(unit)s AND job_date = %(job_date)s"
+        db_engine.execute(sql, {'pid': data["cleaner_id"], 'unit': data["unit_id"],
+                                'job_date': data["job_date"].strftime('%Y-%m-%d')})
+        logging.info(f"Status was changed to 'Canceled'.")
+
+    else:
+        ValueError("""Parameter action has to be one of "add", "change_guests", "change_date" or "cancel".""")
+
 
 if __name__ == "__main__":
     app.run()
