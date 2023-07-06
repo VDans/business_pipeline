55310a42d8cc89e8cbd6c8fc2a2f543dbe4ff6c5
vdans <valen@live.be>
2023-07-06 10:16:25 +0200

    Advancing on the auto messaging feature. Nothing ready

--- messaging.py
+++ messaging.py
@@ -1,6 +1,11 @@
+from twilio.rest import Client
+
+
 class Message:
-    def __init__(self):
-        pass
+    def __init__(self, secrets):
+        self.secrets = secrets
+
+        self.client = Client(self.secrets['twilio']['account_sid'], self.secrets['twilio']['auth_token'])
 
     def send_email(self, target_email: str, body: str):
         """
@@ -12,10 +17,18 @@ class Message:
         """
         Sends a custom Whatsapp message
         """
-        pass
+        response = self.client.messages.create(from_="whatsapp:+436703085269",
+                                               to=target_phone,
+                                               body=body)
+        return response
 
     def send_sms(self, target_phone: str, body: str, title: str = None):
         """
         Sends a custom text message
         """
-        pass
+        response = self.client.messages.create(
+            body=body,
+            from_="+436703085269",
+            to=target_phone
+        )
+        return response
